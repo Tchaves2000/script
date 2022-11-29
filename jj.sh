f2597f42b5259fd1f139622c97059b8664e4a109
Tiago <tiagocb@gmail.com>
2022-11-29 20:41:54 -0300

    ajuste

--- jj.sh
+++ jj.sh
@@ -1,31 +1,48 @@
 #!/bin/bash
+
+# CABEÇALHO DO ARQUIVO DE SAIDA CHAMADO DE output.csv
 echo "hostname, ip, rtt, perda_de_pacotes, status" | tee output.csv 
 
+# NOME DO ARQUIVO DE ENTRADA ips.csv
 filename="ips.csv"
 {
+    # faz a leitura linha a linha separado por "," pegando os valores servidor e ip
     read
     while IFS="," read -r servidor ip
     do 
+        # imprime qual linha esta executando
         echo "${servidor}-${ip}"
+        # imprime qual será o comando de ping e para qual ip
         echo "ping -c3 ${ip}"
+        # guarda o resultado comando de ping
         dados=$(ping -c3 ${ip}) 
 
+        # recorda do resultado, na linha que informa os pacotes perdidos qual foi a %
         packetloss=`echo $dados | grep "packet loss" | awk -F ',' '{print $3}' | awk '{print $1}'`
+        # recorda do resultado, na linha que informa o RTT qual foi o tempo medio (sexto texto separado por "/")
         rrt=`echo $dados | grep "round-trip" | cut -f 6 -d "/"`
         
-        situacao="Circuito degradado"
-        ok='0.0%'
-        nok='100.0%'
+        # TESTES DA QUANTIDADE DE PACOTES PERDIDOS
+        # NÃO CONSEGUI USAR O COMANDO IF ENTÃO USEI O CASE
+        # SITUACAO PADRÃO É CIRCUITO DEGRADADO
+        situacao="ERRO"
         case $packetloss in
             '0.0%') 
-                #echo 1 $packetloss==$ok
+                # CASO SEJA 0.0%
                 situacao="Conectividade ok"
             ;;
             '100.0%')
-                #echo 2 $packetloss==$nok
+                # CASO SEJA 100.0%
                 situacao="Hostname inacessível"
             ;;
+            *)
+                # QUALQUER OUTRO VALOR
+                situacao="Circuito degradado"
+            ;;
         esac
+        # IMPRIME O RESULTADO SEPARADO POR "," NO ARQUIVO DE SAIDA CHAMADO output.csv 
+        # USANDO O COMANDO tee COM O ARGUMANTO -a PARA ANEXAR
         echo "${servidor},${ip},${rrt},${packetloss},${situacao}" | tee -a output.csv 
     done
 } < $filename
+# O COMANDO ACIMA É PARA DIZER QUE O ARQUIVO É A ENTRADA DO BLOCO
